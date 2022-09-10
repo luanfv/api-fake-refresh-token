22dc6c9344fa4212cddcb654b21758f4d755fe5f
luanfv <luanfrancavieira@gmail.com>
2022-09-10 14:35:23 -0300

    chore: save token in refresh token

--- src/request.js
+++ src/request.js
@@ -1,30 +1,36 @@
-const axios = require('axios');
+import 'dotenv/config';
 
-const REFRESH_TOKEN = 'refresh token';
+import axios from 'axios';
+
+let tokenCache = '';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: `${process.env.BASE_URL}:${process.env.PORT}`,
 });
+
+api.defaults.headers.common.Authorization = tokenCache;
 
 api.interceptors.response.use(
   function (response) {
     return response;
   },
   function (error) {
+    // eslint-disable-next-line no-async-promise-executor
     return new Promise(async (resolve, reject) => {
       if (error.response.status === 401) {
         try {
           const response = await api.post('/refresh-token', {
-            refresh_token: REFRESH_TOKEN,
+            refresh_token: process.env.REFRESH_TOKEN,
           });
 
           const { token } = response.data;
 
-          api.defaults.headers.common.Authorization = `Bearer ${token}`;
+          tokenCache = token;
+          api.defaults.headers.common.Authorization = tokenCache;
 
           const refreshRequest = await axios({
             ...error.config,
-            headers: { Authorization: token },
+            headers: { Authorization: tokenCache },
           });
 
           resolve(refreshRequest);
@@ -38,12 +44,19 @@ api.interceptors.response.use(
   }
 );
 
-api
-  .get('/auth')
-  .then((response) => console.log('SUCCESS:', response.data))
-  .catch(() => console.log('FAILURE'));
+async function run() {
+  try {
+    const responseWithoutToken = await api.get('/auth');
+    console.log(
+      'Authentication without valid token and with valid refresh token:',
+      responseWithoutToken.data
+    );
 
-// api
-//   .post('/refresh-token', { refresh_token: 'refresh token' })
-//   .then((response) => console.log('SUCCESS:', response.data))
-//   .catch(() => console.log('FAILURE'));
+    const responseWithToken = await api.get('/auth');
+    console.log('Authentication with valid token:', responseWithToken.data);
+  } catch (err) {
+    console.log('ERROR:', err);
+  }
+}
+
+run();
