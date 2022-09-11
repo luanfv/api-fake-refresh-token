191d869aae3e699478ba256c38dc6273ec73bad6
luanfv <luanfrancavieira@gmail.com>
2022-09-11 10:11:40 -0300

    chore: add jest and write situations

---
+++ src/api.test.js
@@ -0,0 +1,9 @@
+jest.mock('./api');
+
+describe('when requesting the API with Axios', () => {
+  describe('when the token has not expired', () => {});
+
+  describe('when the token expires but has a valid refresh token', () => {});
+
+  describe("when the token expires but doesn't have a valid refresh token", () => {});
+});
