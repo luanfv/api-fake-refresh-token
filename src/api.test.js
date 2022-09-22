c6edc03019f049d28beef2d0b417c747f28d51a4
luanfv <luanfrancavieira@gmail.com>
2022-09-22 18:54:51 -0300

    test: when the refresh request fails

--- src/api.test.js
+++ src/api.test.js
@@ -17,6 +17,7 @@ describe('when requesting with Axios', () => {
 
   describe('when the token is valid', () => {
     beforeAll(() => {
+      api.defaults.headers.common.Authorization = 'Bearer 123456';
       const expectedToken = 'Bearer 123456';
 
       mockApi.onGet('/auth').reply((config) => {
@@ -52,18 +53,14 @@ describe('when requesting with Axios', () => {
 
     describe('when making a GET request', () => {
       it('should return status 200', async () => {
-        const response = await api.get('/auth', {
-          headers: { Authorization: 'Bearer 123456' },
-        });
+        const response = await api.get('/auth');
 
         expect(response.status).toEqual(200);
       });
 
       it('should return message authorized', async () => {
         const expectedResponse = { message: 'authorized' };
-        const response = await api.get('/auth', {
-          headers: { Authorization: 'Bearer 123456' },
-        });
+        const response = await api.get('/auth');
 
         expect(response.data).toEqual(expectedResponse);
       });
@@ -71,13 +68,7 @@ describe('when requesting with Axios', () => {
 
     describe('when making a POST request', () => {
       it('should return status 201', async () => {
-        const response = await api.post(
-          '/todo',
-          { task: 'test' },
-          {
-            headers: { Authorization: 'Bearer 123456' },
-          },
-        );
+        const response = await api.post('/todo', { task: 'test' });
 
         expect(response.status).toEqual(201);
       });
@@ -88,13 +79,7 @@ describe('when requesting with Axios', () => {
           task: 'test',
         };
 
-        const response = await api.post(
-          '/todo',
-          { task: 'test' },
-          {
-            headers: { Authorization: 'Bearer 123456' },
-          },
-        );
+        const response = await api.post('/todo', { task: 'test' });
 
         expect(response.data).toEqual(expectedResponse);
       });
@@ -103,56 +88,60 @@ describe('when requesting with Axios', () => {
 
   describe('when the token expires or is invalid', () => {
     describe('when has a valid refresh token', () => {
+      storageRefreshToken.get.mockImplementation(() => 'abcd');
+
+      const expectedRefreshToken = 'abcd';
+      const expectedToken = 'Bearer 123456';
+
+      beforeEach(() => {
+        api.defaults.headers.common.Authorization = '';
+      });
+
+      beforeAll(() => {
+        mockApi.onPost('/refresh-token').reply((config) => {
+          return new Promise((resolve) => {
+            const responseRefreshToken = JSON.parse(config.data).refresh_token;
+
+            if (responseRefreshToken === expectedRefreshToken) {
+              resolve([200, { token: '123456' }]);
+            }
+
+            resolve([400]);
+          });
+        });
+
+        mockApi.onPost('/todo').reply((config) => {
+          return new Promise((resolve) => {
+            if (expectedToken === config.headers.Authorization) {
+              const body = JSON.parse(config.data);
+
+              if (body && body.task) {
+                resolve([
+                  201,
+                  {
+                    id: 1,
+                    task: body.task,
+                  },
+                ]);
+              }
+            }
+
+            resolve([401, { message: 'unauthorized' }]);
+          });
+        });
+
+        mockApi.onGet('/auth').reply(401, { message: 'unauthorized' });
+      });
+
       describe('when the refresh request is successful', () => {
         beforeAll(() => {
-          storageRefreshToken.get.mockImplementation(() => 'abcd');
-
-          const expectedRefreshToken = storageRefreshToken.get();
-          const expectedToken = 'Bearer 123456';
-
-          mockApi.onPost('/refresh-token').reply((config) => {
-            return new Promise((resolve) => {
-              const responseRefreshToken = JSON.parse(
-                config.data,
-              ).refresh_token;
-
-              if (responseRefreshToken === expectedRefreshToken) {
-                resolve([200, { token: '123456' }]);
-              }
-
-              resolve([400]);
-            });
-          });
-
-          mockApi.onGet('/auth').reply(401);
-
-          mockApi.onPost('/todo').reply((config) => {
-            return new Promise((resolve) => {
-              if (expectedToken === config.headers.Authorization) {
-                const body = JSON.parse(config.data);
-
-                if (body && body.task) {
-                  resolve([
-                    201,
-                    {
-                      id: 1,
-                      task: body.task,
-                    },
-                  ]);
-                }
-              }
-
-              resolve([401]);
-            });
-          });
-
           mockAxios.onGet('/auth').reply((config) => {
             return new Promise((resolve) => {
               if (expectedToken === config.headers.Authorization) {
                 resolve([200, { message: 'authorized' }]);
               }
 
-              resolve([401]);
+              resolve([500]);
             });
           });
 
@@ -172,7 +161,7 @@ describe('when requesting with Axios', () => {
                 }
               }
 
-              resolve([401]);
+              resolve([500]);
             });
           });
         });
@@ -185,9 +174,10 @@ describe('when requesting with Axios', () => {
           });
 
           it('should return message authorized', async () => {
+            const expectedResponse = { message: 'authorized' };
             const response = await api.get('/auth');
 
-            expect(response.data.message).toEqual('authorized');
+            expect(response.data).toEqual(expectedResponse);
           });
         });
 
@@ -211,7 +201,61 @@ describe('when requesting with Axios', () => {
         });
       });
 
-      describe('when the refresh request fails', () => {});
+      describe('when the refresh request fails', () => {
+        beforeAll(() => {
+          mockAxios.onGet('/auth').reply(500);
+
+          mockAxios.onPost('/todo').reply(500);
+        });
+
+        describe('when making a GET request', () => {
+          it('should return status 401', async () => {
+            try {
+              await api.get('/auth');
+
+              expect(1).toEqual(0);
+            } catch (err) {
+              expect(err.response.status).toEqual(401);
+            }
+          });
+
+          it('should return message unauthorized', async () => {
+            const expectedResponse = { message: 'unauthorized' };
+
+            try {
+              await api.get('/auth');
+
+              expect(1).toEqual(0);
+            } catch (err) {
+              expect(err.response.data).toEqual(expectedResponse);
+            }
+          });
+        });
+
+        describe('when making a POST request', () => {
+          it('should return status 401', async () => {
+            try {
+              await api.post('/todo', { task: 'test' });
+
+              expect(1).toEqual(0);
+            } catch (err) {
+              expect(err.response.status).toEqual(401);
+            }
+          });
+
+          it('should return message unauthorized', async () => {
+            const expectedResponse = { message: 'unauthorized' };
+
+            try {
+              await api.post('/todo', { task: 'test' });
+
+              expect(1).toEqual(0);
+            } catch (err) {
+              expect(err.response.data).toEqual(expectedResponse);
+            }
+          });
+        });
+      });
     });
 
     describe('when has a invalid refresh token', () => {});
